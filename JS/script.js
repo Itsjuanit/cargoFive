48a9dc104c3f87b0146c8c742caf1fc11c804dbf
Juan Ignacio Tejada <tejadajuanignacio12@gmail.com>
2022-02-08 02:31:43 -0300

    Challange finish

---
+++ JS/script.js
@@ -0,0 +1,101 @@
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+    usuario: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, 
+    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, 
+    password: /^.{4,12}$/, 
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    telefono: /^\d{5,14}$/,
+    telefonoFijo: /^\d{5,14}$/,
+    solicitud: /[1-9]/g
+}
+
+const campos = {
+    usuario: false,
+    nombre: false,
+    password: false,
+    correo: false,
+    telefono: false,
+    telefonoFijo: false,
+    solicitud: false
+}
+
+const validarFormulario = (e) => {
+    switch (e.target.name) {
+        case "usuario":
+            validarCampo(expresiones.usuario, e.target, 'usuario');
+            break;
+        case "solicitud":
+            validarCampo(expresiones.solicitud, e.target, 'solicitud');
+            break;
+        case "nombre":
+            validarCampo(expresiones.nombre, e.target, 'nombre');
+            break;
+
+        case "correo":
+            validarCampo(expresiones.correo, e.target, 'correo');
+            break;
+        case "telefono":
+            validarCampo(expresiones.telefono, e.target, 'telefono');
+            break;
+        case "telefonoFijo":
+            validarCampo(expresiones.telefonoFijo, e.target, 'telefonoFijo');
+            break;
+    }
+}
+
+const validarCampo = (expresion, input, campo) => {
+    if (expresion.test(input.value)) {
+        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+        campos[campo] = true;
+    } else {
+        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+        campos[campo] = false;
+    }
+}
+
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (campos.usuario && campos.solicitud && campos.correo && campos.telefono && campos.telefonoFijo) {
+        formulario.reset();
+
+        document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+        setTimeout(() => {
+            document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+        }, 7000);
+
+        document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+            icono.classList.remove('formulario__grupo-correcto');
+        });
+    } else {
+        document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+    }
+});
+
+const open = document.getElementById('btn-modal');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('close');
+
+open.addEventListener('click', () => {
+    modal_container.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show');
+});
